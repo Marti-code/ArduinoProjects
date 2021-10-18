9980a7e774100decf504d3444744e51205d2ef41
Marti-code <marti.huza@gmail.com>
2021-10-18 09:48:17 +0200

    added circles

--- Snake/src/main.cpp
+++ Snake/src/main.cpp
@@ -12,20 +12,16 @@
 #define LEFT 4
 #define DOWN 5
 
-//to make it more interesting add points
-//so within half a minute the player have to catch circles
-//the points will be showed at the end
-//and then the game will reset
-//maybe add the best score?
-
 uint8_t blockCount = 0;
-uint8_t circleCount = 0;
+uint32_t points = 0;
+uint32_t t = 0;
+uint32_t t2 = 0;
+
+//1 -> GAME, 2-> GAME OVER
+uint8_t state = 0;
 
 uint8_t rectSide = 7;
-uint8_t circleRadius = 2.5;
-
-int8_t randomX;
-int8_t randomY;
+uint8_t circleRadius = 2;
 
 int8_t coordinateX[17] = {8,15,22,29,36,43,50,57,64,71,78,85,92,99,106,113,120};
 int8_t coordinateY[8] = {7,14,21,28,35,42,49,56};
@@ -83,15 +79,11 @@ void drawRect(uint16_t x, uint16_t y){
 }
 
 void drawFilledCircle(){
-  srand((unsigned)time(0)); 
-  randomX = coordinateX[rand()%17];
-  randomY = coordinateY[rand()%8];
-  
-  circleCoordinates[0] = randomX;
-  circleCoordinates[1] = randomY;
+  circleCoordinates[0] = coordinateX[random(17)];
+  circleCoordinates[1] = coordinateY[random(8)];
 
-  display.drawCircle(randomX, randomY, circleRadius, WHITE);
-  display.fillCircle(randomX, randomY, circleRadius, WHITE);
+  display.drawCircle(circleCoordinates[0], circleCoordinates[1], circleRadius, WHITE);
+  display.fillCircle(circleCoordinates[0], circleCoordinates[1], circleRadius, WHITE);
   display.display();
 }
 
@@ -103,18 +95,26 @@ void drawField(){
   for(uint8_t i = 0; i<blockCount; i++){
     drawRect(SNAKE[i].x, SNAKE[i].y);
   }
-
-  //draw circle
-  if(circleCount == 0){
-    drawFilledCircle();
-    circleCount++;
-  }
 }
 
 void checkCollision(){
+  display.drawCircle(circleCoordinates[0], circleCoordinates[1], circleRadius, WHITE);
+  display.fillCircle(circleCoordinates[0], circleCoordinates[1], circleRadius, WHITE);
+  display.display();
+
   if(SNAKE[0].x == circleCoordinates[0] && SNAKE[0].y == circleCoordinates[1]){
-    circleCount--;
+    drawFilledCircle();
     addBlock();
+    points++;
+  }
+}
+
+void checkGameOver(){
+  for(uint8_t i = 1; i < blockCount; i++){
+    if(SNAKE[0].x == SNAKE[i].x && SNAKE[0].y == SNAKE[i].y){
+      t = millis();
+      state = 2;
+    }
   }
 }
 
@@ -160,6 +160,16 @@ void moveSnake(){
   }
 }
 
+void reset(){
+  memset(SNAKE, 0, 20*sizeof(Block));
+  blockCount = 0;
+  SNAKE[blockCount] =  Block(22,21);
+  blockCount++;
+  addBlock();
+  addBlock();
+  direction = UP;
+}
+
 void setup() {
   Wire.begin();
   serial.begin(9600);
@@ -173,8 +183,12 @@ void setup() {
   memset(SNAKE, 0, 20*sizeof(Block));
   display.clearDisplay();
 
-  //important, don't change the coordinates
-  SNAKE[blockCount] =  Block(22,21);
+  display.setCursor(30, 26);
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+
+  //init snake
+  SNAKE[blockCount] =  Block(22,21); //important, don't change the coordinates
   blockCount++;
   addBlock();
   addBlock();
@@ -183,41 +197,72 @@ void setup() {
   pinMode(RIGHT, INPUT_PULLUP);
   pinMode(LEFT, INPUT_PULLUP);
   pinMode(DOWN, INPUT_PULLUP);
-  move = millis();
+  
+  //generate random circle coordinates
+  randomSeed(analogRead(A0));
+  circleCoordinates[0] = coordinateX[random(17)];
+  circleCoordinates[1] = coordinateY[random(8)];
+
+  t2 = millis();
 }
 
 void loop() {
-  checkCollision();
-  drawField();
+  if(state ==0){
+    display.setCursor(30, 26);
+    display.println("Snake");
+    display.display(); 
 
-  //refresh screen every second
-  if(millis() - move > 1000){
-    moveSnake();
-    move = millis();
+    if(millis() - t2> 3000){
+      move = millis();
+      state = 1;
+    }
   }
+  else if(state == 1){
+    checkGameOver();
+    checkCollision();
+    drawField();
 
-  if(!digitalRead(UP)){
-    if(direction == DOWN) direction = DOWN;
-    else direction = UP;
+    //refresh screen every second
+    if(millis() - move > 1000){
+      moveSnake();
+      move = millis();
+    }
 
-    while(!digitalRead(UP));
-  }
-  if(!digitalRead(RIGHT)){
-    if(direction == LEFT) direction = LEFT;
-    else direction = RIGHT;
+    if(!digitalRead(UP)){
+      if(direction == DOWN) direction = DOWN;
+      else direction = UP;
 
-    while(!digitalRead(RIGHT));
+      while(!digitalRead(UP));
+    }
+    if(!digitalRead(RIGHT)){
+      if(direction == LEFT) direction = LEFT;
+      else direction = RIGHT;
+
+      while(!digitalRead(RIGHT));
+    }
+    if(!digitalRead(LEFT)){
+      if(direction == RIGHT) direction = RIGHT;
+      else direction = LEFT;
+      
+      while(!digitalRead(LEFT));
+    }
+    if(!digitalRead(DOWN)){
+      if(direction == UP) direction = UP;
+      else direction = DOWN;
+      
+      while(!digitalRead(DOWN));
+    }
   }
-  if(!digitalRead(LEFT)){
-    if(direction == RIGHT) direction = RIGHT;
-    else direction = LEFT;
-    
-    while(!digitalRead(LEFT));
-  }
-  if(!digitalRead(DOWN)){
-    if(direction == UP) direction = UP;
-    else direction = DOWN;
-    
-    while(!digitalRead(DOWN));
+  else if(state == 2){
+    display.clearDisplay();
+    display.setCursor(30, 26);
+    display.print("Pkt: ");
+    display.print(points);
+    display.display(); 
+
+    if(millis() - t> 5000){
+        state = 1;
+        reset();
+    }
   }
 }
